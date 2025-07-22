79f7fc3bcb41eaf749f4c18f9865555e48f6cbd4
Timothy Lim <tlim0034@student.monash.edu>
2025-07-22 19:22:53 +1000

    yes

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,25 +1,24 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any unauthenticated user can "create", "read", "update", 
-and "delete" any "Todo" records.
-=========================================================================*/
+
 const schema = a.schema({
-  Todo: a
+  BucketItem: a
     .model({
-      content: a.string(),
+      title: a.string(),
+      description: a.string(),
+      image: a.string(),
     })
-    .authorization((allow) => [allow.guest()]),
+    .authorization((allow) => [allow.owner()]),  // Restrict access to the owner
 });
 
+
 export type Schema = ClientSchema<typeof schema>;
+
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'identityPool',
+    defaultAuthorizationMode: 'userPool',
   },
 });
 
